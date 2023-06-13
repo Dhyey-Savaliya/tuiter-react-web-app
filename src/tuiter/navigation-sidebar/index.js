7059461d719484e17463a5b12f8130360a7df5a4
Dhyey-Savaliya <dhyeysavaliya2000@gmail.com>
2023-06-13 14:01:29 -0400

    a5 REST su23

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -17,7 +17,7 @@ const NavigationSidebar = () => {
    <div className="list-group">
      {!currentUser && <Link className={`list-group-item ${active === "login" ? "active" : ""}`} to="/tuiter/login">   Login   </Link>}
      {!currentUser && <Link className={`list-group-item ${active === "register" ? "active" : ""}`} to="/tuiter/register">Register</Link>}
-     { currentUser && <Link className={`list-group-item ${active === "profile" ? "active" : ""}`} to="/tuiter/profile"> 
+     { currentUser && <Link className={`list-group-item ps-1 text-capitalize col d-flex ${active === "profile" ? "active" : ""}`} to="/tuiter/profile"> 
         <i className={`fa fa-user pe-2 pt-2 fa-solid col-12 col-xl-2 col-xxl-2 fa-solid fa-lg border-0 pb-2 list-group-item text-capitalize ${active === "profile" ? "active" : ""}`}></i>
           <div className={'d-none d-xl-block d-xxl-block ps-4'}>Profile</div>
      </Link>}
